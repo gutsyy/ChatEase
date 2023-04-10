5df143a9df680e40d417a7eef0aade0bd06de6d0
gyuannn <gyuannn1100@gmail.com>
2023-04-10 11:51:30 +0800

    test: git action test

---
+++ preReleaseCheck.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
+
+const checkProjectName = () => {
+  if (packageJson.name === "ChatEase") {
+    throw new Error("Please change the project name in package.json");
+  }
+};
+
+try {
+  checkProjectName();
+} catch (e) {
+  console.error(e.message);
+  process.exit(1);
+}
