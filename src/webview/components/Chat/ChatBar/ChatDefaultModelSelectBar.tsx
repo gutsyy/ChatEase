435489d8d4401c4b681c77c452bf9fed4031dd1b
gyuannn <gyuannn1100@gmail.com>
2024-06-17 00:13:51 +0800

    feat: support gpt-4o model

--- src/webview/components/Chat/ChatBar/ChatDefaultModelSelectBar.tsx
+++ src/webview/components/Chat/ChatBar/ChatDefaultModelSelectBar.tsx
@@ -17,9 +17,9 @@ export function ChatDefaultModelSelectBox() {
 
   return (
     <SegmentedControl
-      defaultValue="gpt-3.5-turbo-0613"
+      defaultValue="gpt-4o-2024-05-13"
       data={[
-        { label: "GPT-4", value: "gpt-4-0613" },
+        { label: "GPT-4o", value: "gpt-4o-2024-05-13" },
         { label: "GPT-3", value: "gpt-3.5-turbo-0613" },
       ]}
       onChange={onValueChange}
--- src/webview/services/openAI/data.ts
+++ src/webview/services/openAI/data.ts
@@ -7,6 +7,8 @@ export const openAIPricing = {
   "gpt-4-0613": 0.06,
   "gpt-4-32k": 0.12,
   "gpt-4-32k-0613": 0.12,
+  "gpt-4o": 0.03,
+  "gpt-4o-2024-05-13": 0.03,
 };
 
 export type OpenAIModels = keyof typeof openAIPricing;
